8dc45d2acb0d9852b11f4f829fb2c546b2c64c65
Guilherme França da Silva <francaguilherme27@gmail.com>
2026-07-07 13:46:35 -0300

    add reviews section with feedback cards and styles

---
+++ avaliações.js
@@ -0,0 +1,144 @@
+const reviews = [
+  {
+    instagram: "@joaocorta",
+    name: "João Victor",
+    photo: "assets/img/reviews/01.webp",
+    comment: "Já faz um tempo que corto aqui. Os caras acertam o corte toda vez, nem preciso explicar muito mais.",
+    stars: 5
+  },
+  {
+    instagram: "@pedrin.021",
+    name: "Pedro Henrique",
+    photo: "assets/img/reviews/02.webp",
+    comment: "Cheguei por indicação de um amigo e agora só corto aqui. Atendimento sempre firmeza.",
+    stars: 5
+  },
+  {
+    instagram: "@leo_sampa",
+    name: "Leonardo Souza",
+    photo: "assets/img/reviews/03.webp",
+    comment: "Degradê ficou na régua. Dá pra ver que os caras têm experiência. 🔥",
+    stars: 5
+  },
+  {
+    instagram: "@caio_na_area",
+    name: "Caio Augusto",
+    photo: "assets/img/reviews/04.webp",
+    comment: "Ambiente tranquilo, papo bom e corte melhor ainda.",
+    stars: 5
+  },
+  {
+    instagram: "@gui.almeidah",
+    name: "Guilherme Almeida",
+    photo: "assets/img/reviews/05.webp",
+    comment: "Primeira vez que vim e já saí marcando o próximo horário.",
+    stars: 5
+  },
+  {
+    instagram: "@brunozl",
+    name: "Bruno Lima",
+    photo: "assets/img/reviews/06.webp",
+    comment: "Preço justo e serviço muito caprichado. Vale cada real.",
+    stars: 5
+  },
+  {
+    instagram: "@rafa.mtz",
+    name: "Rafael Martins",
+    photo: "assets/img/reviews/07.webp",
+    comment: "Gostei porque o barbeiro foi trocando ideia e entendendo exatamente o corte que eu queria.",
+    stars: 5
+  },
+  {
+    instagram: "@diegoprado_",
+    name: "Diego Prado",
+    photo: "assets/img/reviews/08.webp",
+    comment: "Sempre muito bem atendido. Até meu filho já virou cliente.",
+    stars: 5
+  },
+  {
+    instagram: "@henriquefps",
+    name: "Henrique Ferreira",
+    photo: "assets/img/reviews/09.webp",
+    comment: "Não moro tão perto, mas continuo vindo porque o atendimento compensa.",
+    stars: 5
+  },
+  {
+    instagram: "@matheus.rios",
+    name: "Matheus Rios",
+    photo: "assets/img/reviews/10.webp",
+    comment: "Os detalhes da barba ficaram muito bem feitos. Curti demais.",
+    stars: 5
+  },
+  {
+    instagram: "@vini.cardoso",
+    name: "Vinícius Cardoso",
+    photo: "assets/img/reviews/11.webp",
+    comment: "Lugar simples, organizado e com uma energia muito boa.",
+    stars: 5
+  },
+  {
+    instagram: "@lucas.nobrega",
+    name: "Lucas Nóbrega",
+    photo: "assets/img/reviews/12.webp",
+    comment: "Saí pronto pra um casamento. Minha esposa elogiou assim que cheguei em casa 😂",
+    stars: 5
+  },
+  {
+    instagram: "@andrecostta",
+    name: "André Costa",
+    photo: "assets/img/reviews/13.webp",
+    comment: "Já tentei cortar em outros lugares, mas sempre acabo voltando pra cá.",
+    stars: 5
+  },
+  {
+    instagram: "@thiagocx",
+    name: "Thiago Carvalho",
+    photo: "assets/img/reviews/14.webp",
+    comment: "Os caras respeitam o horário e o corte fica sempre do jeito que eu gosto.",
+    stars: 5
+  },
+  {
+    instagram: "@renato_moraes",
+    name: "Renato Moraes",
+    photo: "assets/img/reviews/15.webp",
+    comment: "Barbeiro gente boa demais. Parece que você está conversando com um amigo. 👊🏽",
+    stars: 5
+  },
+  {
+    instagram: "@felipe.ramos_",
+    name: "Felipe Ramos",
+    photo: "assets/img/reviews/16.webp",
+    comment: "Achei a barbearia pelo Google e a nota faz sentido. Serviço excelente.",
+    stars: 5
+  },
+  {
+    instagram: "@edu.alvesz",
+    name: "Eduardo Alves",
+    photo: "assets/img/reviews/17.webp",
+    comment: "Sempre cheio, mas dá pra entender o motivo. O trabalho é muito bom.",
+    stars: 5
+  },
+  {
+    instagram: "@maarceloo",
+    name: "Marcelo Dias",
+    photo: "assets/img/reviews/18.webp",
+    comment: "Atendimento muito bom e corte bem feito. Recomendo pra qualquer um do bairro. 🔥👊🏽",
+    stars: 5
+  },
+  {
+    instagram: "@ricardogms",
+    name: "Ricardo Gomes",
+    photo: "assets/img/reviews/19.webp",
+    comment: "Esperei um pouquinho porque estava movimentado, mas fui muito bem atendido. Voltarei com certeza.",
+    stars: 4
+  },
+  {
+    instagram: "@alex.ferraz",
+    name: "Alex Ferraz",
+    photo: "assets/img/reviews/20.webp",
+    comment: "Gostei bastante do corte. Só marcaria um horário mais cedo da próxima vez porque enche rápido.",
+    stars: 4
+  }
+];
+
+
